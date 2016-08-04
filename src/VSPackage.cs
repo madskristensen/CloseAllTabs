f30c8c4e97b57fa7b784b60cc36ce6e101db8da8
Mads Kristensen <post@madskristensen.net>
2016-08-04 10:43:32 -0700

    Implemented AsyncPackage

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using task = System.Threading.Tasks.Task;
 
 namespace CloseAllTabs
 {
@@ -13,11 +15,14 @@ namespace CloseAllTabs
     [ProvideAutoLoad(UIContextGuids.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideOptionPage(typeof(Options), "Environment", Vsix.Name, 101, 102, true, new string[] { }, ProvidesLocalizedCategoryName = false)]
     [Guid(Vsix.Id)]
-    public sealed class CleanOnClosePackage : Package
+    public sealed class CleanOnClosePackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override async task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            var dte = GetService(typeof(DTE)) as DTE2;
+            var dte = await GetServiceAsync(typeof(DTE)) as DTE2;
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
+
             var options = (Options)GetDialogPage(typeof(Options));
 
             CloseOpenDocuments.Initialize(dte, options);
